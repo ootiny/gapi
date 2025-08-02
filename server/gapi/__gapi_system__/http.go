e3592d0ecf364020a4404e806a8f68577878c8b8
tslearn <tslearn@163.com>
2025-08-03 05:43:56 +0800

    change

---
+++ server/gapi/__gapi_system__/http.go
@@ -0,0 +1,47 @@
+package __gapi_system__
+
+import (
+	"crypto/tls"
+	"net/http"
+)
+
+var gMux = http.NewServeMux()
+
+func ListenAndServe(addr string) error {
+	server := &http.Server{
+		Addr:    addr,
+		Handler: gMux,
+	}
+	return server.ListenAndServe()
+}
+
+func ListenAndServeTLS(addr, certFile, keyFile string) error {
+	server := &http.Server{
+		Addr:    addr,
+		Handler: gMux,
+	}
+	return server.ListenAndServeTLS(certFile, keyFile)
+}
+
+func ListenAndServeTLSWithCert(addr string, certBytes []byte, keyBytes []byte) error {
+	cert, err := tls.X509KeyPair(certBytes, keyBytes)
+	if err != nil {
+		return err
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+	}
+
+	server := &http.Server{
+		Addr:      addr,
+		Handler:   gMux,
+		TLSConfig: tlsConfig,
+	}
+
+	return server.ListenAndServeTLS("", "")
+}
+
+func RegisterHandler(path string, handler http.HandlerFunc) {
+	gMux.HandleFunc(path, handler)
+}
